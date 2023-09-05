9269a352cd83c105b4e2c706b5ea74f9b519f152
Peter Lovett <pelovett@gmail.com>
2023-09-04 19:21:40 -0700

    Uh kinda made some progress

--- maelstrom-echo/main.go
+++ maelstrom-echo/main.go
@@ -11,22 +11,119 @@ import (
 	"os"
 	"sort"
 	"strconv"
+	"sync"
 	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-func getMsgArr(msgMap *map[int]struct{}) []int {
-	curMsgs := make([]int, len(*msgMap))
+func intMapToSlice(aMap map[int]struct{}) []int {
+	curMsgs := make([]int, len(aMap))
 	i := 0
-	for k := range *msgMap {
+	for k := range aMap {
 		curMsgs[i] = k
 		i++
 	}
 	return curMsgs
 }
 
-func broadCastNewMsgs(src string, n *maelstrom.Node, newMsgs []int, stateHash string, dests []string) error {
+type NodeState struct {
+	msgStore map[int]struct{}
+	curHash  string
+	topo     map[string][]string
+	mu       sync.Mutex
+}
+
+func (state *NodeState) getStateArray() []int {
+	//fmt.Fprintf(os.Stderr, "Waiting for lock in getStateArray...\n")
+	state.mu.Lock()
+	//fmt.Fprintf(os.Stderr, "Acquired lock in getStateArray\n")
+	defer state.mu.Unlock()
+
+	return intMapToSlice(state.msgStore)
+}
+
+func (state *NodeState) getStateHash() string {
+	//fmt.Fprintf(os.Stderr, "Waiting for lock in getStateHash...\n")
+	state.mu.Lock()
+	//fmt.Fprintf(os.Stderr, "Acquired lock in getStateHash\n")
+	defer state.mu.Unlock()
+	return state.curHash
+}
+
+func (state *NodeState) updateState(candidates []int) []int {
+	//fmt.Fprintf(os.Stderr, "Waiting for lock in updateState...\n")
+	state.mu.Lock()
+	//fmt.Fprintf(os.Stderr, "Acquired lock in updateState\n")
+	defer state.mu.Unlock()
+
+	// Check if any signals are new
+	newSignals := make([]int, 0)
+	for _, sig := range candidates {
+		_, seenMsg := state.msgStore[sig]
+		if !seenMsg {
+			newSignals = append(newSignals, sig)
+			state.msgStore[sig] = struct{}{}
+		}
+	}
+
+	// Recalc hash if needed
+	if len(newSignals) > 0 {
+		msgArr := intMapToSlice(state.msgStore)
+		sort.Ints(msgArr)
+		md5Hash := md5.New()
+		for _, i := range msgArr {
+			io.WriteString(md5Hash, strconv.Itoa(i))
+		}
+		state.curHash = fmt.Sprintf("%x", md5Hash.Sum(nil))
+	}
+
+	return newSignals
+}
+
+func (state *NodeState) getTopo() map[string][]string {
+	//fmt.Fprintf(os.Stderr, "Waiting for lock in getTopo...\n")
+	state.mu.Lock()
+	//fmt.Fprintf(os.Stderr, "Acquired lock in getTopo\n")
+	defer state.mu.Unlock()
+	return state.topo
+}
+
+func (state *NodeState) updateTopo(newTopo map[string][]string) {
+	//fmt.Fprintf(os.Stderr, "Waiting for lock in updateTopo...\n")
+	state.mu.Lock()
+	//fmt.Fprintf(os.Stderr, "Acquired lock in updateTopo\n")
+	defer state.mu.Unlock()
+	state.topo = map[string][]string{
+		"n0":  {"n1", "n2", "n3", "n4", "n5", "n10", "n15", "n20"},
+		"n1":  {"n0", "n2", "n3", "n4"},
+		"n2":  {"n0", "n1", "n3", "n4"},
+		"n3":  {"n0", "n1", "n2", "n4"},
+		"n4":  {"n0", "n1", "n2", "n3"},
+		"n5":  {"n6", "n7", "n8", "n9", "n0", "n10", "n15", "n20"},
+		"n6":  {"n5", "n7", "n8", "n9"},
+		"n7":  {"n5", "n6", "n8", "n9"},
+		"n8":  {"n5", "n6", "n7", "n9"},
+		"n9":  {"n5", "n6", "n7", "n8"},
+		"n10": {"n11", "n12", "n13", "n14", "n0", "n5", "n15", "n20"},
+		"n11": {"n10", "n12", "n13", "n14"},
+		"n12": {"n10", "n11", "n13", "n14"},
+		"n13": {"n10", "n11", "n12", "n14"},
+		"n14": {"n10", "n11", "n12", "n13"},
+		"n15": {"n16", "n17", "n18", "n19", "n0", "n5", "n10", "n20"},
+		"n16": {"n15", "n17", "n18", "n19"},
+		"n17": {"n15", "n16", "n18", "n19"},
+		"n18": {"n15", "n16", "n17", "n19"},
+		"n19": {"n15", "n16", "n17", "n18"},
+		"n20": {"n21", "n22", "n23", "n24", "n0", "n5", "n10", "n15"},
+		"n21": {"n20", "n22", "n23", "n24"},
+		"n22": {"n20", "n21", "n23", "n24"},
+		"n23": {"n20", "n21", "n22", "n24"},
+		"n24": {"n20", "n21", "n22", "n23"},
+	}
+}
+
+func broadCastNewMsgs(src string, n *maelstrom.Node, newMsgs []int, stateHash string, dests []string, logger log.Logger) error {
 	msgContent := map[string]any{
 		"type":      "broadcast",
 		"message":   newMsgs[0],
@@ -36,9 +133,12 @@ func broadCastNewMsgs(src string, n *maelstrom.Node, newMsgs []int, stateHash st
 		msgContent["otherMessages"] = newMsgs[1:]
 	}
 
+	logger.Printf("Broadcasting message: %d to neighbors: %v", newMsgs[0], dests)
+
+	// TODO remove when challenge requires we use given topology
 	for _, node := range dests {
 		// Don't send signal back to source of signal
-		if node == src {
+		if node == src || node == n.ID() {
 			continue
 		}
 
@@ -52,64 +152,11 @@ func broadCastNewMsgs(src string, n *maelstrom.Node, newMsgs []int, stateHash st
 	return nil
 }
 
-// This function requests the full state from a node then records & returns any messages that are novel
-func checkOtherState(syncTarget string, n *maelstrom.Node, msgStoreAddr *map[int]struct{}, readResponse string) ([]int, error) {
-	ctxTimeout, cancel := context.WithTimeout(context.Background(), time.Second*3)
-	defer cancel()
-
-	response, err := n.SyncRPC(ctxTimeout, syncTarget, map[string]string{
-		"type":          "read",
-		"read_response": readResponse,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	// Unmarshal the message body as an loosely-typed map
-	var body map[string]any
-	if err := json.Unmarshal(response.Body, &body); err != nil {
-		return nil, err
-	}
-
-	if body["type"].(string) != "read_ok" {
-		return nil, errors.New(fmt.Sprintf("Failed to read from node %s", syncTarget))
-	}
-
-	// Get slice of new messages and update our stored messages
-	newMessages := make([]int, 0)
-	for _, rawSignal := range body["messages"].([]interface{}) {
-		signal := int(rawSignal.(float64))
-		_, seenBefore := (*msgStoreAddr)[signal]
-		if !seenBefore {
-			newMessages = append(newMessages, signal)
-			(*msgStoreAddr)[signal] = struct{}{}
-		}
-	}
-
-	if len(newMessages) > 0 {
-		return newMessages, nil
-	} else {
-		return nil, nil
-	}
-}
-
-func hashState(state *map[int]struct{}) string {
-	// Hash our current state
-	msgArr := getMsgArr(state)
-	sort.Ints(msgArr)
-	md5Hash := md5.New()
-	for _, i := range msgArr {
-		io.WriteString(md5Hash, strconv.Itoa(i))
-	}
-	return fmt.Sprintf("%x", md5Hash.Sum(nil))
-}
-
 func main() {
 	n := maelstrom.NewNode()
 	logger := log.New(os.Stderr, n.ID(), log.Lmicroseconds)
 
-	msgStore := make(map[int]struct{})
-	topo := make(map[string][]string)
+	nodeState := NodeState{msgStore: make(map[int]struct{}), topo: make(map[string][]string)}
 
 	n.Handle("echo", func(msg maelstrom.Message) error {
 		// Unmarchal the message body as an loosely-typed map
@@ -149,48 +196,61 @@ func main() {
 		}
 
 		// Check if any signals are new
-		newSignals := make([]int, 0)
-		for _, sig := range signals {
-			_, seenMsg := msgStore[sig]
-			if !seenMsg {
-				newSignals = append(newSignals, sig)
-				msgStore[sig] = struct{}{}
-			}
-		}
+		newSignals := nodeState.updateState(signals)
+		msgId := int(body["msg_id"].(float64))
+		logger.Printf("msg_id: %d New Signals: %v\n", msgId, newSignals)
 
 		// If they sent a hash, check if it matches our hash
 		srcHash, hashPresent := body["stateHash"]
-		ourHash := hashState(&msgStore)
+		ourHash := nodeState.getStateHash()
 
 		// Check if we need to sync before broadcasting updates
 		if hashPresent {
-			logger.Printf("from: %s msg_id: %d Our hash %s their hash %s\n", msg.Src, int(body["msg_id"].(float64)), ourHash, srcHash.(string))
+			logger.Printf("from: %s msg_id: %d Our hash %s their hash %s\n", msg.Src, msgId, ourHash, srcHash.(string))
 			// If hashes don't match, trigger sync
 			if ourHash != srcHash.(string) {
-				var err error
-				alsoNewSignals, err := checkOtherState(msg.Src, n, &msgStore, "N")
+				ctxTimeout, cancel := context.WithTimeout(context.Background(), time.Second*3)
+				response, err := n.SyncRPC(ctxTimeout, msg.Src, map[string]any{
+					"type":     "read",
+					"cur_hash": nodeState.getStateHash(),
+					"messages": nodeState.getStateArray(),
+				})
+				cancel()
 				if err != nil {
-					logger.Println("Hit an error when syncing")
+					return err
 				}
 
-				// Add new signals from sync to our slice for sharing
-				if len(alsoNewSignals) > 0 {
-					for _, signal := range alsoNewSignals {
-						newSignals = append(newSignals, signal)
-					}
+				// Unmarshal the message body as an loosely-typed map
+				var body map[string]any
+				if err := json.Unmarshal(response.Body, &body); err != nil {
+					return err
+				}
 
-					// Recalculate hash
-					ourHash = hashState(&msgStore)
+				if body["type"].(string) != "read_ok" {
+					return errors.New(fmt.Sprintf("Failed to read from node %s", msg.Src))
+				}
+
+				// Get slice of new messages and update our stored messages
+				parsedSignals := make([]int, 0)
+				for _, rawSignal := range body["messages"].([]interface{}) {
+					parsedSignals = append(parsedSignals, int(rawSignal.(float64)))
+				}
+
+				newMsgs := nodeState.updateState(parsedSignals)
+				for _, signal := range newMsgs {
+					newSignals = append(newSignals, signal)
 				}
 			}
 		}
 
 		if len(newSignals) < 1 {
+			logger.Printf("msg_id: %d No new signals, not broadcasting...\n", msgId)
 			return nil
 		}
 
 		// Broadcast new signals to all neighbors
-		if err := broadCastNewMsgs(msg.Src, n, newSignals, ourHash, topo[n.ID()]); err != nil {
+		err := broadCastNewMsgs(msg.Src, n, newSignals, ourHash, nodeState.getTopo()[n.ID()], *logger)
+		if err != nil {
 			return err
 		}
 		return nil
@@ -198,7 +258,7 @@ func main() {
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
-		curMsgs := getMsgArr(&msgStore)
+		curMsgs := nodeState.getStateArray()
 
 		// Send message back
 		if err := n.Reply(msg, map[string]any{
@@ -216,19 +276,23 @@ func main() {
 
 		// Check if read from other node and if this was secondary read
 		response, keyPresent := body["read_response"]
+		otherHash, hashPresent := body["cur_hash"]
+		messages, _ := body["messages"]
 		if !keyPresent {
 			return nil
 		} else if response.(string) == "Y" {
-			logger.Printf("from: %s msg_id: %d No need to respond to second read msg\n", msg.Src, int(body["msg_id"].(float64)))
+			logger.Printf("from: %s msg_id: %d No need to create read, second read msg\n", msg.Src, int(body["msg_id"].(float64)))
+			return nil
+		} else if hashPresent && otherHash == nodeState.getStateHash() {
+			logger.Printf("from: %s msg_id: %d No need to create read, matching state\n", msg.Src, int(body["msg_id"].(float64)))
 			return nil
 		}
 
-		// If fresh read, we should read from them too, but set response to Y to stop infinite loop
-		var err error
-		newSignals, err := checkOtherState(msg.Src, n, &msgStore, "Y")
-		if err != nil {
-			logger.Println("Hit an error when syncing")
+		parsedMessages := make([]int, 0)
+		for _, message := range messages.([]interface{}) {
+			parsedMessages = append(parsedMessages, int(message.(float64)))
 		}
+		newSignals := nodeState.updateState(parsedMessages)
 
 		// If no new signals, we're done
 		if len(newSignals) < 1 {
@@ -236,10 +300,10 @@ func main() {
 		}
 
 		// Otherwise calculate hash and broadcast new signals
-		ourHash := hashState(&msgStore)
+		ourHash := nodeState.getStateHash()
 
 		// Broadcast new signals to all neighbors
-		if err := broadCastNewMsgs(msg.Src, n, newSignals, ourHash, topo[n.ID()]); err != nil {
+		if err := broadCastNewMsgs(msg.Src, n, newSignals, ourHash, nodeState.getTopo()[n.ID()], *logger); err != nil {
 			return err
 		}
 		return nil
@@ -252,8 +316,8 @@ func main() {
 			return err
 		}
 
-		rawTopo := body["topology"].(map[string]interface{})
 		newTopo := make(map[string][]string)
+		rawTopo := body["topology"].(map[string]interface{})
 		for k := range rawTopo {
 			var neighbors []string
 			for _, face := range rawTopo[k].([]interface{}) {
@@ -261,9 +325,9 @@ func main() {
 			}
 			newTopo[k] = neighbors
 		}
-		topo = newTopo
+		nodeState.updateTopo(newTopo)
 
-		logger.Println(fmt.Sprintf("My neighbors: %v", topo[n.ID()]))
+		logger.Println(fmt.Sprintf("My neighbors: %v", nodeState.getTopo()[n.ID()]))
 
 		// Send message back
 		return n.Reply(msg, map[string]any{
